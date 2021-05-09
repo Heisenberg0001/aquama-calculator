26a25b4c29ce8c571aeefb44218e911143f02cd4
Heisenberg0001 <39101574+Heisenberg0001@users.noreply.github.com>
2021-05-09 21:10:42 +0400

    adding select, output and responsive design

--- script.js
+++ script.js
@@ -13,21 +13,65 @@ const aquamaPerMonthPeople = 0.17;
 
 calculateBtn.addEventListener('click', calculateCosts);
 
-function validateMyForm() {
-    return false;
-}
 function calculateCosts() {
-    debugger;
-    if(squareArea.value && numberOfPeople.value && numberOfMonths.value) {
-        debugger;
+    const condition = squareArea.selectedIndex !== 0 && numberOfPeople.selectedIndex !== 0 && numberOfMonths.selectedIndex !== 0
+
+    if(condition) {
         const totalCostCalculated = (parseFloat(squareArea.value) *  parseFloat(numberOfMonths.value) * costPerSquare)
-                                    + (parseFloat(numberOfPeople.value) * parseFloat(numberOfMonths.value) * costPerPerson);
+            + (parseFloat(numberOfPeople.value) * parseFloat(numberOfMonths.value) * costPerPerson);
 
         const aquamaCostCalculated = (parseInt(squareArea.value) *  parseInt(numberOfMonths.value) * aquamaPerMonthSquare)
-                                    + (parseInt(numberOfPeople.value) * parseInt(numberOfMonths.value) * aquamaPerMonthPeople);
+            + (parseInt(numberOfPeople.value) * parseInt(numberOfMonths.value) * aquamaPerMonthPeople);
 
         totalCost.innerHTML = totalCostCalculated.toFixed().toString();
         aquamaCost.innerHTML = aquamaCostCalculated.toFixed().toString();
         output.style.display = 'flex';
     }
 }
+function styleSelect() {
+    $('select').each(function(){
+        var $this = $(this), numberOfOptions = $(this).children('option').length;
+
+        $this.addClass('select-hidden');
+        $this.wrap('<div class="select"></div>');
+        $this.after('<div class="select-styled"></div>');
+
+        var $styledSelect = $this.next('div.select-styled');
+        $styledSelect.text($this.children('option').eq(0).text());
+
+        var $list = $('<ul />', {
+            'class': 'select-options'
+        }).insertAfter($styledSelect);
+
+        for (var i = 0; i < numberOfOptions; i++) {
+            $('<li />', {
+                text: $this.children('option').eq(i).text(),
+                rel: $this.children('option').eq(i).val()
+            }).appendTo($list);
+        }
+
+        var $listItems = $list.children('li');
+
+        $styledSelect.click(function(e) {
+            e.stopPropagation();
+            $('div.select-styled.active').not(this).each(function(){
+                $(this).removeClass('active').next('ul.select-options').hide();
+            });
+            $(this).toggleClass('active').next('ul.select-options').toggle();
+        });
+
+        $listItems.click(function(e) {
+            e.stopPropagation();
+            $styledSelect.text($(this).text()).removeClass('active');
+            $this.val($(this).attr('rel'));
+            $list.hide();
+        });
+
+        $(document).click(function() {
+            $styledSelect.removeClass('active');
+            $list.hide();
+        });
+
+    });
+}
+styleSelect();
